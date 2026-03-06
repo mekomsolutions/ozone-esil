fb68314beb547dbbecc9565d36d08e1ef7e8bdde
Emmanuel Nyachoke <achachiez@gmail.com>
2026-03-06 09:24:52 +0300

    MG-29: Add eip-analytics-dhis2 route and it's configs

---
+++ configs/analytics/dhis2-sync/queries/hiv-testing-facility-a.sql
@@ -0,0 +1,14 @@
+SELECT
+  'GqKaojXhPdg' AS data_set,
+  TO_CHAR(CAST(obs.obs_date AS TIMESTAMP), 'YYYYMM') AS period,
+  COUNT(*) AS tests_performed,
+  COUNT(CASE WHEN obs.val_quantity > 18.0 THEN 1 END) AS tests_positive
+FROM diagnostic_report_flat dr
+JOIN observation_flat obs ON dr.result_obs_id = obs.id
+JOIN (
+    SELECT DISTINCT id, gender FROM patient_flat
+) p ON dr.patient_id = p.id
+WHERE obs.obs_date > :#lastSyncTimestamp
+  AND dr.code_code = '718-7'
+GROUP BY TO_CHAR(CAST(obs.obs_date AS TIMESTAMP), 'YYYYMM')
+ORDER BY period
