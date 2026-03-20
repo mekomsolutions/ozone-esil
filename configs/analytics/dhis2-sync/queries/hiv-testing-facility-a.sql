a3e47eecde228db4ece30bec02d87c5526b73e48
Emmanuel Nyachoke <achachiez@gmail.com>
2026-03-20 07:49:17 +0300

    MG-29: Filter our test query by facility_org_id

--- configs/analytics/dhis2-sync/queries/hiv-testing-facility-a.sql
+++ configs/analytics/dhis2-sync/queries/hiv-testing-facility-a.sql
@@ -10,5 +10,6 @@ JOIN (
 ) p ON dr.patient_id = p.id
 WHERE obs.obs_date >= DATE_TRUNC('month', CAST(:#lastSyncTimestamp AS TIMESTAMP))
   AND dr.code_code = '68961-2'
+  AND dr.facility_org_id = 'f5a738b0-b85d-4c1a-bc29-21f097c4fc36'
 GROUP BY TO_CHAR(CAST(obs.obs_date AS TIMESTAMP), 'YYYYMM')
 ORDER BY period
